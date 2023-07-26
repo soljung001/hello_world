14bf1d23a7b4854889dfde843541d51a27dd4754
sojulng <soljung72101@gmail.com>
2023-07-26 15:26:41 -0700

    added if_practice

---
+++ if_practice.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 15:17:52 2023
+
+@author: solju
+"""
+
+n = input ("Enter a number:")
+if n==1:
+    print("True")
+else:
+    print("False")
---
+++ list_practice.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 15:02:35 2023
+
+@author: solju
+"""
+
+list =['apple', 'bana', 'cat', 'dog']
+print(list)
